1f84977447b183fd38f3bd68c73099ec54ac33a9
arthurhzna <arthurhozana123@gmail.com>
2026-04-19 13:38:53 +0700

    aku pabrik

---
+++ 3. Factories/2_interface_factory.go
@@ -0,0 +1,23 @@
+type Person interface {
+	SayHello()
+}
+
+type person struct {
+	name string
+	age int
+}
+
+func (p *person) SayHello() {
+	fmt.Println("Hello, my name is", p.name)
+}
+
+func NewPerson(name string, age int) Person {
+	return &person{name, age} 
+}
+// Returns a pointer to the Person struct.
+// NewPerson returns a struct that has the SayHello method.
+
+func main() {
+	p := NewPerson("John", 20)
+	p.SayHello()
+}
